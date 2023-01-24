b131d18b39d00f15aade6837d9a8972a24c826a8
JRingterm <ghwns3712@naver.com>
2023-01-24 19:35:45 +0900

    Add requirements.txt

--- pokemons/serializers.py
+++ pokemons/serializers.py
@@ -4,7 +4,7 @@ from .models import Pokemon
 class LookupSerializer(serializers.ModelSerializer): #전체조회 시리얼라이저
     class Meta:
         model = Pokemon
-        fields = ('id', 'name', 'teratype', 'stats', 'item', 'description')
+        fields = ('id', 'name', 'teratype', 'ability', 'stats', 'item', 'description')
 
 class DetailCreateSerializer(serializers.ModelSerializer): #상세조회, 생성 시리얼라이저
     class Meta:
