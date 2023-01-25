8411b8b1c997fe43950e57ccf60a359606ec3363
JRingterm <ghwns3712@naver.com>
2023-01-25 22:10:43 +0900

    Add PUT and DELETE

--- pokemons/views.py
+++ pokemons/views.py
@@ -20,7 +20,21 @@ class PokemonsAPIView(APIView): #전체조회 뷰
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PokemonAPIView(APIView): #상세조회 뷰 (1개 가져오기)
-    def get(self, request, pk):
+    def get(self, request, pk): #데이터 가져오기
         pokemon = get_object_or_404(Pokemon, id=pk)
         serializer = DetailCreateSerializer(pokemon)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk): #데이터 수정하기
+        pokemon = Pokemon.objects.get(id=pk)
+        serializer = DetailCreateSerializer(pokemon, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk): #데이터 삭제
+        pokemon = Pokemon.objects.get(id=pk)
+        pokemon.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
